9006d1fe1314acee68c581ecd9f329f1ccae603f
boryana.yordanova <boryana.yordanova@flatrocktech.com>
2023-12-19 17:46:53 +0200

    mapping questions

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Quiz from './components/Quiz.jsx';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Quiz />
+        </>
+    )
+}
+
+export default App;
