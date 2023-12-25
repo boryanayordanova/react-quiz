d9de899f8fc29cf618c57ab34f7cad238e99095d
Boryana <boryana.yourdanova@gmail.come>
2023-12-25 23:53:39 +0200

    nan results

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+export default function QuestionTimer({timeout, onTimeout, mode}) {
+    
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+
+    useEffect(() => {
+        console.log("Setting timeout");
+        const timer = setTimeout(  
+            onTimeout, timeout)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [timeout, onTimeout])
+
+
+    useEffect(() => {
+        console.log("Setting interval");
+        const interval = setInterval(() => {
+            setRemainingTime((prevReminingTime) => prevReminingTime -100)
+        }, 100)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, []);
+
+
+
+    return (
+        <progress id="question-time" max={timeout} value={remainingTime} className={mode}/>
+    )
+}
